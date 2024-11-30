d863c9ce16915f2d18e6a169b0c1793d237b7423
ken <kenken4016@gmail.com>
2024-11-29 23:39:20 -0600

    modify actions

--- src/lane_follow/src/action.py
+++ src/lane_follow/src/action.py
@@ -10,13 +10,6 @@ class action_unit():
         # Waypoints topic
         self.waypoints = rospy.Subscriber('/lane_detection/waypoints', Int16MultiArray, self.waypoints_callback, queue_size=1)
 
-        # Node states
-        self.left_line = Line(n=5)
-        self.right_line = Line(n=5)
-        self.detected = False
-        self.hist = True
-        self.turn = "front"
-
     def waypoints_callback(self, data):
         waypoints = waypoints_callback_helper(data)
         print("waypoints", waypoints)
